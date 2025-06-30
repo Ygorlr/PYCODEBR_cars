730cfe69d449efef7945671b05a19bc46676a32f
Ygor Rocha <86207163+Ygorlr@users.noreply.github.com>
2025-06-30 16:29:44 -0300

    AgoraVai

--- projeto_carros/Scripts/activate.bat
+++ projeto_carros/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\PC-240-W10\projetos\carros\projeto_carros
+set VIRTUAL_ENV=C:\Users\testeadmin\project\cars\projeto_carros
 
 if not defined PROMPT set PROMPT=$P$G
 
