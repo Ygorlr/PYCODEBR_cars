0ba90b21f4df0d1be15804a40f0dde1b9b1e673d
Ygor Rocha <86207163+Ygorlr@users.noreply.github.com>
2025-07-07 20:57:38 -0300

    20250707-1.2

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+#from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
+#from django.views import View
+from django.views.generic import ListView, CreateView, DetailView
 
 
 # Create your views here.
@@ -18,7 +19,7 @@ from django.views import View
         {'cars': cars }
     )'''
 
-class CarsView(View):
+'''class CarsView(View):
 
     def get(self, request):
         cars = Car.objects.all().order_by('model')
@@ -31,7 +32,19 @@ class CarsView(View):
             request,
             'cars.html',
             {'cars': cars }
-        )
+        )'''
+    
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
 
 '''def new_car_view(request):
     if request.method == 'POST':
@@ -43,7 +56,7 @@ class CarsView(View):
         new_car_form = CarModelForm()
     return render(request, 'new_car.html', { 'new_car_form': new_car_form })'''
 
-class NewCarView(View):
+'''class NewCarView(View):
 
     def get(self, request):
         new_car_form = CarModelForm()
@@ -54,4 +67,14 @@ class NewCarView(View):
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+        return render(request, 'new_car.html', { 'new_car_form': new_car_form })'''
+    
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
